e4f2cb41dcfcafa3fa8cb63e724dd397e00b1eeb
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-08-29 23:19:06 -0500

    fixing typescript bugs

---
+++ src/mui/Button.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const material_1 = require("@mui/material");
+const react_1 = __importDefault(require("react"));
+const Button = ({ variant, color, href, startIcon, endIcon, onClick, text, sx }) => {
+    const props = {
+        variant: variant,
+        color: color,
+        href: href,
+        onClick: onClick,
+        startIcon: startIcon,
+        endIcon: endIcon,
+        sx: sx,
+    };
+    return react_1.default.createElement(material_1.Button, Object.assign({}, props), text);
+};
+exports.default = Button;
+// * 'text' for anchor like look
+// * 'contained' for for normal button
+// * 'outlined' for outlined button
