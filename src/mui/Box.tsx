423b0d9caf6f814042acc3d47747ad6a51b031dc
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-08-29 23:17:49 -0500

    fixing typescript bugs

--- src/mui/Box.tsx
+++ src/mui/Box.tsx
@@ -1,11 +1,18 @@
 import { Box as MuiBox } from "@mui/material";
 import React from "react";
 
-const Box = ({ className, display, children }) => {
+interface BoxProps {
+  className?: string;
+  display?: string;
+  children: React.ReactNode;
+}
+
+const Box: React.FC<BoxProps> = ({ className, display, children }) => {
   return (
     <MuiBox className={className} display={display}>
       {children}
     </MuiBox>
   );
 };
+
 export default Box;
